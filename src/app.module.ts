e50624a7bfe24be4b8665483151ce05391d375e7
Bagas Tri Wibowo <bagaskuhaku@gmail.com>
2024-03-18 02:14:06 +0700

    feature: add validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FileUploadModule } from './file_upload/file_upload.module';
-import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { ValidationModule } from './validation/validation.module';
 
 @Module({
-  imports: [FileUploadModule],
+  imports: [FileUploadModule, ValidationModule.forRoot(true)],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ValidationService } from 'src/validation/validation.service';
+import { z } from 'zod';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) { }
+    constructor(
+        private readonly userService: UserService,
+        private readonly validationService: ValidationService
+    ) { }
 
     @Post('login')
     async login(@Body('email') email: string, @Body('password') password: string) {
+        const emailSchema = z.string().min(6).max(40).email()
+        const passwordSchema = z.string().min(6).max(40)
+
+        const emailResult = this.validationService.validate(emailSchema, email)
+        const passwordResult = this.validationService.validate(passwordSchema, password)
+
         const user = await this.userService.login(email, password);
         if (user) {
             // Authentication successful
